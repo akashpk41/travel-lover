309381c91abe0538a766e08bf91e4a311133e74f
akashpk41 <akashpk84100@gmail.com>
2022-02-17 10:05:40 +0600

    validation is added

--- js/app.js
+++ js/app.js
@@ -2,8 +2,7 @@
 // function for getting input field and get input field number.
 function getInputValue(inputId){
    const inputField = document.getElementById( inputId + '-field');
-   const inputValue = parseInt(inputField.value);
-   
+   const inputValue = parseInt(inputField.value); 
    return inputValue
 }
 
@@ -16,13 +15,22 @@ calculateBtn.addEventListener('click',function(){
   const foodCost = getInputValue('food');
   const rentCost = getInputValue('rent');
   const clothesCost = getInputValue('clothes');
-
+// debugger;
   const totalExpenses = foodCost + rentCost + clothesCost;
   const balance       =  myIncome - totalExpenses
-  console.log(totalExpenses);
-//   totalExpense update on html 
-document.getElementById('total-expenses').innerText = totalExpenses
-document.getElementById('balance').innerText = balance
 
+   const myBalance = document.getElementById('income-field').value;
 
+   // validation 1. if input field value is negative. 
+   if( myIncome < 0 || foodCost < 0 || rentCost < 0 || clothesCost <0 ){
+      alert('sorry! nagative number are not alowed ')
+   } 
+      // validation 2. if my expenses amount greater than my money
+  else if(totalExpenses < myBalance){
+     //   totalExpense update on html 
+      document.getElementById('total-expenses').innerText = totalExpenses
+      document.getElementById('balance').innerText = balance
+   } else{
+      alert("Sorry! you don't have enough balance. ")
+   }
 })
